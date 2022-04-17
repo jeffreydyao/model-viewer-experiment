9ad9a3b27f0b501df94e6fc5a672ebbb6cdc4244
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-04-17 19:22:17 +1000

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import "@fontsource/spline-sans"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+    <>
+      <script
+        type='module'
+        src='https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js'
+        strategy='beforeInteractive'
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
